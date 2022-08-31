c4c05bf571043ad94cf93a27f322cf04712b5812
upneto <upneto@gmail.com>
2022-08-31 19:58:04 -0300

    head

--- src/main/java/br/com/fiap/fmba/controller/HealthController.java
+++ src/main/java/br/com/fiap/fmba/controller/HealthController.java
@@ -3,18 +3,15 @@ package br.com.fiap.fmba.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.fiap.fmba.controller.payload.LoginRequest;
 
 @RestController
 @RequestMapping("/health")
 public class HealthController {
 
-	@GetMapping(produces="application/json", consumes="application/json")
-	public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+	@GetMapping
+	public ResponseEntity<?> login() {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
