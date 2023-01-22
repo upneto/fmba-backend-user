10de1dad7e854a9d1ccc6702a302cc7498a919d9
Ulisses Pereira da Silva Neto <upneto@gmail.com>
2023-01-22 16:37:38 -0300

    conecao mq heroku

--- src/main/java/br/com/fiap/fmba/controller/api/AccessController.java
+++ src/main/java/br/com/fiap/fmba/controller/api/AccessController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.fmba.controller;
+package br.com.fiap.fmba.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
